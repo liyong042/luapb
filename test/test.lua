e1e38dcedfce31fb3b5979ea1569aa92e1ba21e2
liyong042 <40205311+liyong042@users.noreply.github.com>
2018-06-26 21:24:01 +0800

    Update test.lua

--- test/test.lua
+++ test/test.lua
@@ -3,14 +3,15 @@ require("luapb");
 
 print('------------start------------------- ' )
 
+pb.set_log( 3 )        	--LOG_ERROR
 pb.register_path( "./" )
 pb.register_file( "persion.proto" )
 
 pb_buf = pb.encode( "Person", { name="test1", id=104, email="163.com" }   )
 pb_msg = pb.decode( "Person", pb_buf    )
 
- for k, v in pairs( pb_msg ) do
+for k, v in pairs( pb_msg ) do
 	print( k, v )
- end 
+end 
 
 print('-----------------end------------------- ' )
