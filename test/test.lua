50211cee0ec0531d15d777a71bcc6082ce6a1094
liyong042 <40205311+liyong042@users.noreply.github.com>
2018-06-26 20:56:38 +0800

    Add files via upload
    luapb test lua

---
+++ test/test.lua
@@ -0,0 +1,16 @@
+
+require("luapb");
+
+print('------------start------------------- ' )
+
+pb.register_path( "./" )
+pb.register_file( "persion.proto" )
+
+pb_buf = pb.encode( "Person", { name="test1", id=104, email="163.com" }   )
+pb_msg = pb.decode( "Person", pb_buf    )
+
+ for k, v in pairs( pb_msg ) do
+	print( k, v )
+ end 
+
+print('-----------------end------------------- ' )
