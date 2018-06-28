c8f9e686d5520be24a0b174287743c0d328ac163
liyong042 <40205311+liyong042@users.noreply.github.com>
2018-06-28 10:44:47 +0800

    Update test.lua
    support enum

--- test/test.lua
+++ test/test.lua
@@ -1,17 +1,23 @@
 
 require("luapb");
-
 print('------------start------------------- ' )
 
 pb.set_log( 3 )        	--LOG_ERROR
 pb.register_path( "./" )
 pb.register_file( "persion.proto" )
 
-pb_buf = pb.encode( "Person", { name="test1", id=104, email="163.com" }   )
+pb_buf = pb.encode( "Person", { name="test1", id=104, email="163.com"，phone={ {number=444,type=1},{ number=5555,type="WORK" } } }   )
 pb_msg = pb.decode( "Person", pb_buf    )
 
-for k, v in pairs( pb_msg ) do
-	print( k, v )
+local function dump( tb )
+	for k, v in pairs( tb ) do
+		if type(v) == 'table' then
+			dump(v )
+		else 
+		   print( k, v )
+		end
+	end 
 end 
+dump（ pb_msg ）
 
 print('-----------------end------------------- ' )
