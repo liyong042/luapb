7a33a0cf2db80f3527908802dcc82e4c672b4e94
liyong042 <40205311+liyong042@users.noreply.github.com>
2018-06-28 11:05:47 +0800

    Update lua_pb.cpp

--- src/lua_pb.cpp
+++ src/lua_pb.cpp
@@ -1,12 +1,7 @@
 
 #include <string>
-#include <sstream>
-#include <memory>
 #include "lua_pb.h"
 #include "lua_log.h"
-
-#include <google/protobuf/message.h>
-#include <google/protobuf/descriptor.h>
 #include <google/protobuf/descriptor.pb.h>
 #include <google/protobuf/dynamic_message.h>
 #include <google/protobuf/compiler/importer.h>
