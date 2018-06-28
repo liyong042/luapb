221a5e10bfbd1a2cca7c1c8acc4c91eca763c680
liyong042 <40205311+liyong042@users.noreply.github.com>
2018-06-28 10:53:55 +0800

    Update test.lua

--- test/test.lua
+++ test/test.lua
@@ -6,7 +6,7 @@ pb.set_log( 3 )        	--LOG_ERROR
 pb.register_path( "./" )
 pb.register_file( "persion.proto" )
 
-pb_buf = pb.encode( "Person", { name="test1", id=104, email="163.com"，phone={ {number=444,type=1},{ number=5555,type="WORK" } } }   )
+pb_buf = pb.encode( "Person", { name="test1", id=104, email="163.com"，phone={ {number=444,type=1},{ number="5555",type="WORK" } } }   )
 pb_msg = pb.decode( "Person", pb_buf    )
 
 local function dump( tb )
